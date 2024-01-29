6019fab9cf36f07cf1d82da9be1d02dc31ec50bd
nilldiggonto <moinulantu@gmail.com>
2024-01-29 11:47:23 +0000

    chat room created

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.utils.text import slugify
 
 # Create your models here.
+class ChatRoom(models.Model):
+    name        = models.CharField(max_length=255)
+    slug        = models.SlugField(unique=True,null=True,blank=True)
+    description = models.TextField(null=True, blank=True)
+    active      = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        return super(ChatRoom, self).save(*args, **kwargs)    
+    def __str__(self):
+        return self.name
