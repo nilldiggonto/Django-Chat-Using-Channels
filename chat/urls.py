23e4662eb3a88b4bae0569388e5456336f7800ac
nilldiggonto <moinulantu@gmail.com>
2024-01-29 12:35:42 +0000

    channels updated

--- chat/urls.py
+++ chat/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
-from .views import indexView
+from .views import indexView,roomsView,roomDetailsView
 urlpatterns = [
     path('', indexView, name='index'),
+    path('rooms/', roomsView, name='rooms'),
+
+    path('room/<slug>/', roomDetailsView, name='room-details'),
 ]
--- chat/views.py
+++ chat/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
+from .models import ChatRoom
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def indexView(request):
     return render(request, 'chat/homepage.html')
+
+@login_required(login_url='/login/')
+def roomsView(request):
+    rooms = ChatRoom.objects.all()
+    return render(request, 'chat/rooms.html',{'rooms':rooms})
+
+@login_required(login_url='/login/')
+def roomDetailsView(request,slug):
+    room = ChatRoom.objects.get(slug=slug)
+    return render(request, 'chat/room.html',{'room':room})
