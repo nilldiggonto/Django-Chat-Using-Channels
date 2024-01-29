9906ea89a381e6682bf04c5c45b2475304d04864
nilldiggonto <moinulantu@gmail.com>
2024-01-29 10:28:13 +0000

    homepage done

--- chat/views.py
+++ chat/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def indexView(request):
-    return render(request, 'chat/base.html')
+    return render(request, 'chat/homepage.html')
